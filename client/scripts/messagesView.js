7b6b500e06283d8ae8aa659b7d014f3a70c9354b
MarissaShieh <marissa.shieh@gmail.com>
2019-03-01 19:00:10 -0800

    Implement ability to append each message to the DOM

--- client/scripts/messagesView.js
+++ client/scripts/messagesView.js
@@ -3,9 +3,17 @@ var MessagesView = {
   $chats: $('#chats'),
 
   initialize: function() {
+   Parse.readAll((data)=>{console.log(data)});
+   //send data to message
+   //$chats.children.
+   //console.log($.ajax.args[0][0].success);
   },
 
-  render: function() {
+  renderMessage: function(message) {
+    //template the messages
+    console.log(MessageView.render(message));
+    this.$chats.append(MessageView.render(message));
+    console.log(this.$chats)
   }
 
 };
