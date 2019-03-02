07078b246e5cf3385ce2b43b1e516bdce75b3a12
MarissaShieh <marissa.shieh@gmail.com>
2019-03-01 21:05:42 -0800

    Deflect XSS if <script> is included in the roomname

--- client/scripts/rooms.js
+++ client/scripts/rooms.js
@@ -2,10 +2,12 @@ var Rooms = {
     returnRoom: function(arrOfObjs) {
         console.log("in the return room function")
         for (let obj of arrOfObjs) {
-            if (obj.roomname) {
+            if (obj.roomname) {;
+                if (obj.roomname.includes("<script>")) {
+                    obj.roomname = 'General';
+                }
                 RoomsView.renderRoom(obj.roomname);
             }
-            continue;
         }
     }
     
