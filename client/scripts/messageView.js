33e463b50cea4952eca48f2c0e9e5568f034cfb3
Marissa <marissa.shieh@gmail.com>
2019-03-02 20:14:29 -0800

    Change friend toggle so ALL messages from your friend will be toggled

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -2,7 +2,12 @@ var MessageView = {
 
   render: _.template(`
       <div class="chat">
-        <button class="username notFriend" type = "button" onclick="Friends.toggleStatus(this, this.innerHTML)"> <%-username%> </button>
+        <button class="username notFriend <%-username%>" type = "button" 
+        onclick='
+          var sameFriend = document.querySelectorAll(".<%-username%>");
+          for (var i = 0; i<sameFriend.length; i++) {
+            Friends.toggleStatus(sameFriend[i], this.innerHTML);
+          }'> <%-username%> </button>
         <div class="text"> <%-text%> </div>
         <div class="roomname"> <%-roomname%> </div>
       </div>
